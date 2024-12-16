2b3544e00fda04f8aca26370d4ce4ade8eea762d
yohanis-tadese <yohanistadese06@gmail.com>
2024-12-16 12:52:35 +0300

    first push the initial projects

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+
+dotenv.config();
+
+const __dirname = path.resolve();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  // res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.status(404).json({ sucess: "false", message: "this routes not found" });
+});
+
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
